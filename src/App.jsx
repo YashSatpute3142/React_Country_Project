692d80f1eb4c8b18df165860cfc9dd5f709ce817
Yash Satpute <yashsatpute2006@gmail.com>
2026-07-08 19:53:46 +0530

    Creating Layout navbar completion Header Footer Componant

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Country } from "./pages/Country";
+import { Home } from "./pages/Home";
+import { AppLayout } from "./componants/Layout/AppLayout";
+import { ErrorPage } from "./pages/ErrorPage";
+import "./App.css"
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout />,
+    errorElement:<ErrorPage />,
+    children:[
+  {
+    path:"/",
+    element: <Home />
+  },
+  {
+    path:"about",
+    element: <About />
+  },
+  {
+    path:"country",
+    element: <Country />
+  },
+  {
+    path:"contact",
+    element: <Contact />
+  },
+] 
+  },
+  
+])
+
 const App = () => {
-  return <h1>Hellow React Project</h1>
+  return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App;
