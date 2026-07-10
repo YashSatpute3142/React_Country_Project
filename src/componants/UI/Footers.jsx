10079099c647def777591bed51f365dfebd2e600
Yash Satpute <yashsatpute2006@gmail.com>
2026-07-10 19:43:00 +0530

    adding contact page  and Footer section

--- src/componants/UI/Footers.jsx
+++ src/componants/UI/Footers.jsx
@@ -1,3 +1,69 @@
+import footerContent from "../../api/footerApi.json";
+import { IoCallSharp } from "react-icons/io5";
+import { MdPlace } from "react-icons/md";
+import { TbMailPlus } from "react-icons/tb";
+import {NavLink} from "react-router-dom"
+
 export const Footers = () => {
-    return <h1>Hello Footers</h1>
+
+    const footerIcon = {
+        MdPlace: <MdPlace />,
+        IoCallSharp: <IoCallSharp />,
+        TbMailPlus: <TbMailPlus />,
+    }
+
+    return (
+        <footer className="footer-section">
+            <div className="container grid grid-three-cols">
+                {
+                    footerContent.map((curData, index) => {
+                        const {icon, title, details} = curData;
+                        return(
+                            <div className="footer-contact" key={index}>
+                                <div className="icon">{footerIcon[icon]}</div>
+                                <div className="footer-contact-text">
+                                    <p>{title}</p>
+                                    <p>{details}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="copyright-area">
+                <div className="container">
+                    <div className="grid grid-two-cols">
+                        <div className="copyright-text">
+                            <p>
+                                Copyright &copy; 2026, All Right Reserved
+                                <NavLink to="https://YashSatpute.com" target="_blank">Yash</NavLink>
+                            </p>
+                        </div>
+
+                        <div className="footer-menu">
+                            <ul>
+                                <li>
+                                    <NavLink to="/">Home</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink 
+                                    to="https://www.instagram.com/?hl=en"
+                                    target="_blank">Social</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink 
+                                    to="https://github.com/YashSatpute3142/React_Country_Project"
+                                    target="_blank">Source Code</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/contact">Contact</NavLink>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,21 @@
 export const Contact = () => {
+
+
+    const handleFormSubmit = (formData) => {
+        console.log(formData.entries());
+        const formInputData = Object.fromEntries(formData.entries())
+        console.log(formInputData);
+        
+        
+    }
+
     return (
         <section className="section-contact">
             <h2 className="container-title">
                 COntact Us
             </h2>
-            <form action="">
+            <div className="contact-wrapper container">
+            <form action={handleFormSubmit}>
                 <input 
                 type="text"
                 required 
@@ -30,7 +41,11 @@ export const Contact = () => {
                 rows="10"
                 placeholder="Enter Your Message"
                 ></textarea>
+
+                <button type="submit" value="send">Send</button>
             </form>
+
+            </div>
         </section>
     )
 }
